f47349ecb42345d6102b94e8c98ec7e1b3ad6b4c
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-04-25 22:09:04 +0700

    feature: SystemTextJson Benchmark

---
+++ Benchmarks/SystemTextJsonBenchmark.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Jobs;
+using System.Text.Json;
+
+namespace Benchmarks;
+
+[MemoryDiagnoser(false)]
+[HideColumns(Column.RatioSD, Column.AllocRatio)]
+[SimpleJob(RuntimeMoniker.Net80)]
+[SimpleJob(RuntimeMoniker.Net90)]
+public class SystemTextJsonBenchmark
+{
+    private SampleModel model;
+    private string json;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        model = new SampleModel();
+        json = "{\"Id\":1,\"UniqueId\":\"fcd03978-0562-4166-bad9-55605e9d5ace\",\"Name\":\"test_name\",\"CreatedAt\":\"2024-04-25T09:39:10.6768869Z\"}";
+    }
+
+    [Benchmark(Baseline = true)]
+    public void Serialize() => JsonSerializer.Serialize(model);
+
+    [Benchmark]
+    public void Deserialize() => JsonSerializer.Deserialize<SampleModel>(json);
+}
+
+
+public class SampleModel
+{
+    public int Id { get; set; } = 1;
+
+    public Guid UniqueId { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; } = "test_name";
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
